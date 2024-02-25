4d2e699b4774a8cc5cbfe55420778c045c541ef9
lukamil1998 <luka_milanovic@rocketmail.com>
2024-02-25 16:11:37 +0100

    Update

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -12,6 +12,7 @@ export const PROJECTS: Project[] = [
       "https://t4.ftcdn.net/jpg/04/85/17/33/360_F_485173339_rHVWOWEwZneJpHqgFNiYJLSPQg7hNoAA.jpg",
     alt: "Weather API App",
   },
+
   {
     id: 2,
     title: "To Do List Web App",
